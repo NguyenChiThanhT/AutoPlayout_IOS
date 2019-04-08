ea4e8da918b8c9c90fa98c2c7b05c481771c2dc4
loginfbandroid.git <nguyennhoc@bitbucket.org>
2019-04-08 16:26:40 +0700

    update new code bai1

---
+++ bai_1/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  bai_1
+//
+//  Created by NGUYEN CHI THANH on 3/12/19.
+//  Copyright © 2019 NGUYEN CHI THANH. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ bai_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bai_1
+//
+//  Created by NGUYEN CHI THANH on 3/12/19.
+//  Copyright © 2019 NGUYEN CHI THANH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
